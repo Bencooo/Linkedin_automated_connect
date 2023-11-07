c84076a9a5202e3b2a836bfb366bf98c677f467b
Bentalha Aymen <>
2023-11-07 10:36:58 +0100

    search, connect, and open profile

--- parameters.py
+++ parameters.py
@@ -1,6 +1,6 @@
 file_name = 'connections.csv'
-linkedin_username = 'bentalha270@outlook.fr'
-linkedin_password = 'Bentalha27'
+linkedin_username = 'othman.bentalha@gmail.com'
+linkedin_password = 'OTHman270!'
 keywords = 'Freelance,consultant,developpeur ios'
 ignore_list = 'Person1,Person2'
 till_page = 10
